5dc07c4569e74d7b71a639df4c96e4bffbedce56
TakammotoYurie <happyyuupy@gmail.com>
2021-12-21 02:18:42 +0900

    add: 文字列や空を弾くvalidationを追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,15 @@ class Book < ApplicationRecord
   has_many :book_authors
   has_many :authors, through: :book_authors
 
-  #scope :costly, -> { where("price > ?", 3000) }
-  #scope :written_about, -> { where("name like ?", "%#{theme}%") }
+  validates :name, presence: true
+  validates :name, length: { maximum: 25 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validate do |book|
+    if book.name.include?("exercise")
+      book.errors[:name] << "I don't like exersise."
+    end
+  end
+
+  scope :costly, -> { where("price > ?", 3000) }
+  scope :written_about, -> { where("name like ?", "%#{theme}%") }
 end
